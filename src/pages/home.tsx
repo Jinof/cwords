1b3c89c9d98a732e2c6b6d976184b38023a1cae9
Jinof <1474121785@qq.com>
2020-08-22 01:33:19 +0800

    :sparkles: 添加 announcement

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,6 +3,7 @@ import React from "react";
 export function Home() {
   return <div>
     <Header />
+    <Announcement />
   </div>
 }
 
@@ -14,6 +15,18 @@ function Header() {
   </div>
 }
 
+function Announcement() {
+  return <div>
+    <div style={styles.announcementBorder}>
+      <p>
+        Readme!!!<br />
+        In this game you should find the synonymous.<br />
+        Your scores is base on the time and the number of successful matches.<br />
+      </p>
+    </div>
+  </div>
+}
+
 const styles = {
   headerText: {
     // Why add `as "center"`/ `as "const"` ?
@@ -21,6 +34,17 @@ const styles = {
     // it can't just be any string, you can cast it to the more specific type.
     // Both works.
     // textAlign: "center" as "center",
-    textAlign: "center" as const
+    textAlign: "center" as const,
+    fontSize: "26px",
+    fontWeight: "bold" as const,
+    fontFamily: "arial"
+  },
+  announcementBorder: {
+    // position: "absolute" as const,
+    marginLeft: "44px",
+    marginTop: "27px",
+    maxWidth: "287px",
+    maxHeight: "177px",
+    fontSize: "20px",
   }
 }
